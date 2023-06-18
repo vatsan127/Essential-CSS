22b0c9ac5c507d4dc87b9aff1d644570745d4cb8
vatsan127 <110617159+vatsan127@users.noreply.github.com>
2023-06-18 12:51:10 +0530

    final commit -sidebar

--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -1,8 +1,9 @@
 const toggleBtn = document.querySelector(".sidebar-toggle-btn");
 const sidebar = document.querySelector(".sidebar-list");
+const sidebarItems = document.querySelector(".sidebar-item ");
 
 function handleSidebar() {
-  sidebar.classList.toggle("active");
+  sidebar.classList.toggle("active-sideBar");
 }
 
 toggleBtn.addEventListener("click", handleSidebar);
