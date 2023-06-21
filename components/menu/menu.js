50f67107d96329e7f0236136f9cbf964aa48713e
vatsan127 <110617159+vatsan127@users.noreply.github.com>
2023-06-21 14:10:06 +0530

    test commit

--- components/menu/menu.js
+++ components/menu/menu.js
@@ -121,3 +121,9 @@ const data = [
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
   },
 ];
+
+function loadContent() {
+  data.map(item =>)
+}
+
+window.addEventListener("DOMContentLoaded", loadContent);
