eb955b4ade90497228de6e2cd4886e57473c4d59
vatsan127 <110617159+vatsan127@users.noreply.github.com>
2023-06-21 07:31:38 +0530

    anime pics updated

---
+++ components/menu/menu.js
@@ -0,0 +1,44 @@
+const data = [
+  {
+    img: "../../img/anime-character-img/naruto.png",
+    name: "Naruto",
+    class: "Genin",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
+  },
+  {
+    img: "../../img/anime-character-img/sasuke.jpg",
+    name: "Sasuke",
+    class: "Genin",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
+  },
+  {
+    img: "../../img/anime-character-img/sakura.png",
+    name: "Sakura",
+    class: "Genin",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
+  },
+  {
+    img: "../../img/anime-character-img/kakashi.png",
+    name: "Kakashi",
+    class: "Jonin",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
+  },
+  {
+    img: "../../img/anime-character-img/jiraiya.jpg",
+    name: "Jiraiya",
+    class: "Sanin",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
+  },
+  {
+    img: "../../img/anime-character-img/tsunade.png",
+    name: "Tsunade",
+    class: "Sanin",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
+  },
+];
