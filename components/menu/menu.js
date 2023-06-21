6ed1666d4880910f0dac1dff676c308bd6a5a30c
vatsan127 <110617159+vatsan127@users.noreply.github.com>
2023-06-21 08:13:37 +0530

    data created - menu

--- components/menu/menu.js
+++ components/menu/menu.js
@@ -1,43 +1,122 @@
 const data = [
   {
     img: "../../img/anime-character-img/naruto.png",
-    name: "Naruto",
+    name: "naruto",
     class: "Genin",
     review:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
   },
   {
     img: "../../img/anime-character-img/sasuke.jpg",
-    name: "Sasuke",
+    name: "sasuke",
     class: "Genin",
     review:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
   },
   {
     img: "../../img/anime-character-img/sakura.png",
-    name: "Sakura",
+    name: "sakura",
     class: "Genin",
     review:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
   },
   {
     img: "../../img/anime-character-img/kakashi.png",
-    name: "Kakashi",
+    name: "kakashi",
     class: "Jonin",
     review:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
   },
+
+  {
+    img: "../../img/anime-character-img/hinata.png",
+    name: "hinata",
+    class: "Genin",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
+  },
+  {
+    img: "../../img/anime-character-img/ino.png",
+    name: "ino",
+    class: "Genin",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
+  },
+  {
+    img: "../../img/anime-character-img/shikamaru.png",
+    name: "shikamaru",
+    class: "Genin",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
+  },
+  {
+    img: "../../img/anime-character-img/choji.png",
+    name: "choji",
+    class: "Genin",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
+  },
+
   {
     img: "../../img/anime-character-img/jiraiya.jpg",
-    name: "Jiraiya",
+    name: "jiraiya",
     class: "Sanin",
     review:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
   },
   {
     img: "../../img/anime-character-img/tsunade.png",
-    name: "Tsunade",
-    class: "Sanin",
+    name: "tsunade",
+    class: "Sanin Hokage",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
+  },
+  {
+    img: "../../img/anime-character-img/orochimaru.png",
+    name: "orochimaru",
+    class: "Sanin Hokage",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
+  },
+  {
+    img: "../../img/anime-character-img/itachi.png",
+    name: "itachi",
+    class: "Jonin",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
+  },
+  {
+    img: "../../img/anime-character-img/madara.png",
+    name: "madara",
+    class: "Founder",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
+  },
+  {
+    img: "../../img/anime-character-img/hashirama.png",
+    name: "hashirama",
+    class: "Founder Hokage",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
+  },
+  {
+    img: "../../img/anime-character-img/tobirama.png",
+    name: "tobirama",
+    class: "Hokage",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
+  },
+  {
+    img: "../../img/anime-character-img/hiruzen.png",
+    name: "hiruzen",
+    class: "Hokage",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
+  },
+  {
+    img: "../../img/anime-character-img/minato.png",
+    name: "minato",
+    class: "Hokage",
     review:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit fuga consectetur dolores non minima facere possimus nostrum sapiente reiciendis veritatis.",
   },
